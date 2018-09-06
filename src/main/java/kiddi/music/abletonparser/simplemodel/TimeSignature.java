0ca560e0d067776ab2e28b0e1bf5d7a599c27f52
Kai Liebscher <kai-l@gmx.de>
2018-09-06 17:47:22 +0200

    cleanup

--- src/main/java/kiddi/music/abletonparser/simplemodel/TimeSignature.java
+++ src/main/java/kiddi/music/abletonparser/simplemodel/TimeSignature.java
@@ -1,18 +1,17 @@
 package kiddi.music.abletonparser.simplemodel;
 
 public class TimeSignature {
-    final private int beats;
-    final private int noteValue;
+    private final int beats;
+    private final int noteValue;
 
     // Ableton encodes time signatures as log2(noteValue) * 99 + beats - 1. This constructor decodes an Ableton time signature into the two separate values.
     public TimeSignature(String abletonSignature) {
         int abletonSignatureInt = Integer.parseInt(abletonSignature);
         int x = abletonSignatureInt % 99;
-        int y = (abletonSignatureInt-x)/99;
+        int y = (abletonSignatureInt - x) / 99;
 
-        this.beats = x+1;
+        this.beats = x + 1;
         this.noteValue = (int) Math.pow(2, y);
-        System.out.println();
     }
 
     public String toString() {
